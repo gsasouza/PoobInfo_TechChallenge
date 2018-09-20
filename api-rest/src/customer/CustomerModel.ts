5c1a6be9ead7f20a1cfd54979c3a70deabf7968c
Gabriel Souza <gabrie.ssouza@gmail.com>
2018-09-19 22:59:59 -0300

    feat(customer): add customer model

---
+++ api-rest/src/customer/CustomerModel.ts
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose';
+import autoIncrement from 'mongoose-auto-increment';
+import * as yup from 'yup';
+
+export interface ICustomer {
+  name: string;
+  rewardsNumber: string;
+  companyId: number;
+  createDate: Date;
+  dob: Date;
+  email: string;
+}
+
+export const CustomerCreateValidationSchema = yup.object().shape({
+  name: yup.string().required(),
+  companyId: yup.string().required(),
+  email: yup.string().email().required(),
+  dob: yup.string().required(),
+  rewardsNumber: yup.string().required(),
+});
+
+export const CustomerUpdateValidationSchema = yup.object().shape({
+  name: yup.string(),
+  companyId: yup.string(),
+  email: yup.string().email(),
+  dob: yup.string(),
+  rewardsNumber: yup.string(),
+});
+
+const Schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    rewardsNumber: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    dob: {
+      type: Date,
+      require: true,
+    }
+  },
+  {
+    timestamps: {
+      createdAt: 'createDate',
+    },
+  },
+);
+
+Schema.plugin(autoIncrement.plugin, { model: 'Customer', field: 'customerId' });
+
+
+export default mongoose.model('Customer', Schema);
