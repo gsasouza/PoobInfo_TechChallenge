befd24c4b3dcd27305ef3afaa444bc6e1e66f166
Gabriel Souza <gabrie.ssouza@gmail.com>
2018-09-20 01:43:38 -0300

    feat(cors): allow cors on api

--- api-rest/src/app.ts
+++ api-rest/src/app.ts
@@ -1,10 +1,12 @@
 import Koa from 'koa';
 import koaBody from 'koa-body';
+import cors from 'kcors';
 
 import { routerMiddleware, responseMiddleware } from './middlewares';
 
 const app = new Koa();
 
+app.use(cors());
 app.use(koaBody());
 app.use(responseMiddleware);
 app.use(routerMiddleware.routes());
